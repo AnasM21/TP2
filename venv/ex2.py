68dad682f6e7949c067f5415882693bce84e0f6b
ANAS MOHAMMADI <MOHAMMADI_ANAS@emsi-edu.ma>
2021-03-22 19:35:26 +0100

    hum

--- venv/ex2.py
+++ venv/ex2.py
@@ -1,20 +1,22 @@
-from collections import Counter
-
+# from collections import Counter
 c = input("saisir votre chaine    ")
-print("la chaine de crt est", c)
-# question 1
+print("--------------------------")
+# question 1-------------------------
 # à chaque fois on teste sur un caractere different
 if "z" in c:
+    
     print("ce caractere existe!")
 else:
     print("ce caractere n'exsite pas!")
-# question 2
-print("le nbr de caractere est", c.count('e'))
-# question 3
+print("--------------------------")
+# question 2-------------------------------
+print("le nbr de caractere desiré est", c.count('e'))
+print("--------------------------")
+# question 3----------------------------------
 txt = c[::-1]
 print(txt)
-
-# question 4
+print("--------------------------")
+# question 4---------------------------------
 """def ispalindrome(s):
     return s == s[::-1]
 
@@ -27,19 +29,54 @@ if txt == c:
 else:
     print("chaine  non palindrome")
 """
-# question 5
+print("--------------------------")
+# question 5-------------------------------------------
 def compteur_mots(s):
     return len(c.split())
 
 
 print("le nbr de mots est", compteur_mots(c))
+print("--------------------------")
+# questions 6---------------------------------------------
 
-# questions 6
-
+"""
 def nb_occurence(t):
-    nbocc = Counter(c)
 
+    nbocc = Counter(c)
+    
+    
     return nbocc
 
 print("Le nombre d'occurence de chaque caractere est :\n "
       + str(nb_occurence(c)))
+"""
+# q6-----------------------------
+def nb_occurrences(t):
+    dictionary = {}
+
+    i = 0
+    for i in range(len(t)):
+        if t[i] not in dictionary:
+            if t.count(t[i]) > 1:
+                dictionary[t[i]] = t.count(t[i])
+    return dictionary
+
+print(nb_occurrences(c))
+print("--------------------------")
+# Q7-------------------------------------------------------
+def nb_occurrences2(t):
+    dictionary = {}
+    i = 0
+    for i in range(len(t)):
+        if t[i] not in dictionary:
+            listchar = []
+            x = t[i]
+            if t.count(t[i]) > 1:
+                for i in range(len(t)):
+                    if t[i] == x:
+                        listchar.append(i)
+                dictionary[x] = listchar
+    return dictionary
+
+print(nb_occurrences2(c))
+print("--------------------------")
